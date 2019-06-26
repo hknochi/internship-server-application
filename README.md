1b1eed6a4f1cfe1f69f76ee5fd87e276b4338966
Henri Knochenhauer <hknochi@gmail.com>
2019-06-26 12:13:23 +0200

    update stuff

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # internship-server-application
 template for server application deployed on heroku 
+
+
+### API
+
+```bash
+curl -XPOST -H "Content-Type:application/json" -v https://floating-garden-54811.herokuapp.com/user/register -d '{
+  "username" : "Test11",
+  "password" : "Test11Test",
+  "firstName" : "Tester",
+  "lastName" : "Tester",
+  "gender" : "female",
+  "yearOfBirth" : 1967
+}'
+```
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,16 @@
 insert into user_profile (username, password, first_name, last_name, gender, year_of_birth) values
 ( 'apfelkuchen', 'geburtstagmeineroma', 'Mario', 'Oiram', 'diverse', 1900);
+insert into user_profile (username, password, first_name, last_name, gender, year_of_birth) values
+( 'birnenkompott88', 'geburtstagmeineroma', 'Patrick', 'der Große', 'male', 1956);
+insert into user_profile (username, password, first_name, last_name, gender, year_of_birth) values
+( 'kleiderschrank', 'geburtstagmeineroma', 'Ingrid', 'des Grauens', 'female', 2011);
+insert into user_profile (username, password, first_name, last_name, gender, year_of_birth) values
+( 'bratwurst', 'geburtstagmeineroma', 'Peter', 'Lustig', 'male', 1934);
+insert into user_profile (username, password, first_name, last_name, gender, year_of_birth) values
+( 'waschbecken', 'geburtstagmeineroma', 'Gisella', 'Rothe', 'female', 1988);
 
 insert into message (msg_id, transmitter_username, receiver_username, msg_content, send_time) values
-( 998877, 'Kleiderschrank', 'Waschbecken', 'Dreh mal bitte den Hahn ab und zieh den Stöpsel.', 1561375194);
+( 998877, 'kleiderschrank', 'waschbecken', 'Dreh mal bitte den Hahn ab und zieh den Stöpsel.', 1561375194);
 
 
 
